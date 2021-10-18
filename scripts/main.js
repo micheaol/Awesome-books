bed686a26cd520dbdb5cd69d2d71e8929cf51676
micheaol <micheaol80@gmail.com>
2021-10-18 23:01:16 +0100

    fix: fix linters errors

--- scripts/main.js
+++ scripts/main.js
@@ -11,8 +11,7 @@ const bookTitle = getMyElement('#title-id');
 const bookAuthor = getMyElement('#author-id');
 const bookParent = getMyElement('.books');
 
-let bookArray = [
-];
+const bookArray = [];
 
 function Book(title, author) {
   this.title = title;
@@ -20,48 +19,49 @@ function Book(title, author) {
 }
 
 function saveLocalStorage() {
-  let key = `${bookTitle.value} + ${bookAuthor.value}`;
+  const key = `${bookTitle.value} + ${bookAuthor.value}`;
   localStorage.setItem(key, JSON.stringify(bookArray));
 }
 
 function addBook(e) {
   e.preventDefault();
-  let newBook = new Book(bookTitle.value, bookAuthor.value);
+  const newBook = new Book(bookTitle.value, bookAuthor.value);
   bookArray.push(newBook);
   saveLocalStorage();
-  location.reload();
+  window.location.reload();
 }
 
 addBookForm.addEventListener('submit', addBook);
 
 function showBook() {
-  for (key in localStorage) {
-    const dataFromLoca = JSON.parse(localStorage.getItem(key));
-    if (dataFromLoca) {
-      dataFromLoca.forEach((book) => {
-        let bookDiv = createMyElement('div');
-        bookDiv.className = 'book';
-        let bookTitle = createMyElement('h3');
-        bookTitle.textContent = book.title;
-        let bookAuthor = createMyElement('p');
-        bookAuthor.textContent = book.author;
-        let removeBtn = createMyElement('button');
-        removeBtn.type = 'button';
-        removeBtn.id = book.title;
-        removeBtn.textContent = 'Remove';
-        removeBtn.addEventListener('click', (e) => {
-          let key = `${bookTitle.textContent} + ${bookAuthor.textContent}`;
-          console.log(key);
-          localStorage.removeItem(key);
-          e.target.parentNode.remove();
-        })
-        let seperator = createMyElement('hr');
-        bookDiv.appendChild(bookTitle);
-        bookDiv.appendChild(bookAuthor);
-        bookDiv.appendChild(removeBtn);
-        bookDiv.appendChild(seperator);
-        bookParent.prepend(bookDiv);
-      });
+  for (const key in localStorage) {
+    if (localStorage.hasOwnProperty.call(localStorage, key)) {
+      const dataFromLoca = JSON.parse(localStorage.getItem(key));
+      if (dataFromLoca) {
+        dataFromLoca.forEach((book) => {
+          const bookDiv = createMyElement('div');
+          bookDiv.className = 'book';
+          const bookTitle = createMyElement('h3');
+          bookTitle.textContent = book.title;
+          const bookAuthor = createMyElement('p');
+          bookAuthor.textContent = book.author;
+          const removeBtn = createMyElement('button');
+          removeBtn.type = 'button';
+          removeBtn.id = book.title;
+          removeBtn.textContent = 'Remove';
+          removeBtn.addEventListener('click', (e) => {
+            const key = `${bookTitle.textContent} + ${bookAuthor.textContent}`;
+            localStorage.removeItem(key);
+            e.target.parentNode.remove();
+          });
+          const seperator = createMyElement('hr');
+          bookDiv.appendChild(bookTitle);
+          bookDiv.appendChild(bookAuthor);
+          bookDiv.appendChild(removeBtn);
+          bookDiv.appendChild(seperator);
+          bookParent.prepend(bookDiv);
+        });
+      }
     }
   }
 }
