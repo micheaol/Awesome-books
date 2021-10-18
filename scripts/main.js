85957b589636469054f683de938b607436e02d10
micheaol <micheaol80@gmail.com>
2021-10-18 17:44:38 +0100

    feat: Create a collection that keeps a list of books in an array of object

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+
+let bookArray = [
+    {
+        title: "Truth",
+        author: 'Michael Oladele',
+    }
+    {
+        title: "Keep in touch",
+        author: 'Michael Ajogba',
+    }
+    {
+        title: "Light",
+        author: 'Matthew Michael-Oladele',
+    }
+    {
+        title: "Truth",
+        author: 'Michael Oladele',
+    }
+];
+
+
