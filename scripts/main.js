db276808312738289355a0e2eea934e9d0223fc9
micheaol <micheaol80@gmail.com>
2021-10-18 20:29:07 +0100

    feat: Create a function to add a new book to the collection, with title and author.

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,27 @@
+function createMyElement(para) {
+  return document.createElement(para);
+}
 
-let bookArray = [
-    {
-        title: "Truth",
-        author: 'Michael Oladele',
-    }
-    {
-        title: "Keep in touch",
-        author: 'Michael Ajogba',
-    }
-    {
-        title: "Light",
-        author: 'Matthew Michael-Oladele',
-    }
-    {
-        title: "Truth",
-        author: 'Michael Oladele',
-    }
-];
+function getMyElement(para) {
+  return document.querySelector(para);
+}
 
+const addBookForm = getMyElement('form');
+const bookTitle = getMyElement('#title-id');
+const bookAuthor = getMyElement('#author-id');
 
+let bookArray = [];
+
+function Book(title, author) {
+  this.title = title;
+  this.author = author;
+}
+
+function addBook(e) {
+  e.preventDefault();
+  let newBook = new Book(bookTitle.value, bookAuthor.value);
+  console.log(bookTitle.value);
+  bookArray.push(newBook);
+}
+
+addBookForm.addEventListener('submit', addBook);
