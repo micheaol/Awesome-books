480373be7c53ae45c8e8e2cafc8ce3fb52e58cde
micheaol <micheaol80@gmail.com>
2021-10-19 22:43:33 +0100

    feat: add remove book logic

--- scripts/main.js
+++ scripts/main.js
@@ -11,33 +11,32 @@ const bookTitle = getMyElement('#title-id');
 const bookAuthor = getMyElement('#author-id');
 const bookParent = getMyElement('.books');
 
-const bookArray = [];
-
-// function Book(title, author) {
-//   this.title = title;
-//   this.author = author;
-// }
-
 class Book {
   constructor(title, author) {
     this.title = title;
     this.author = author;
   }
+
+  removeBook() {
+    const key = `${this.title} + ${this.author}`;
+    localStorage.removeItem(key);
+  }
 }
+/* eslint max-classes-per-file: ["error", 2] */
 
 class Books {
   constructor() {
     this.books = [];
   }
+
   addBook(title, author) {
-    let book = new Book(title, author);
+    const book = new Book(title, author);
     this.books.push(book);
     return book;
   }
-  
 }
+
 const newBook = new Books();
-console.log(newBook);
 
 function saveLocalStorage() {
   const key = `${bookTitle.value} + ${bookAuthor.value}`;
@@ -47,7 +46,6 @@ function saveLocalStorage() {
 function addBook(e) {
   e.preventDefault();
   newBook.addBook(bookTitle.value, bookAuthor.value);
-  //bookArray.push(newBook);
   saveLocalStorage();
   window.location.reload();
 }
@@ -57,10 +55,8 @@ addBookForm.addEventListener('submit', addBook);
 function showBook() {
   Object.keys(localStorage).forEach((key) => {
     const dataFromLoca = JSON.parse(localStorage.getItem(key));
-    console.log(dataFromLoca.books);
     if (dataFromLoca.books) {
-      dataFromLoca.books.forEach(book => {
-        console.log(book);
+      dataFromLoca.books.forEach((book) => {
         const bookDiv = createMyElement('div');
         bookDiv.className = 'book';
         const bookTitle = createMyElement('h3');
@@ -72,8 +68,8 @@ function showBook() {
         removeBtn.id = book.title;
         removeBtn.textContent = 'Remove';
         removeBtn.addEventListener('click', (e) => {
-          const key = `${bookTitle.textContent} + ${bookAuthor.textContent}`;
-          localStorage.removeItem(key);
+          const book = new Book(bookTitle.textContent, bookAuthor.textContent);
+          book.removeBook();
           e.target.parentNode.remove();
         });
         const seperator = createMyElement('hr');
